af48029855217c4b9b129fd5517dca1bae1932a6
Rashed-Alom <rashedalom001@gmail.com>
2019-11-23 15:06:19 +0600

    Network architecture 2

--- project/config.py
+++ project/config.py
@@ -7,6 +7,7 @@ import os
 
 nb_train_samples = 50000
 nb_test_samles = 300000
+nb_classes = 10
 
 img_size = 32
 img_channel = 3
--- project/src/my_model.py
+++ project/src/my_model.py
@@ -4,26 +4,53 @@ File: Network architecture for classifing cifer-10 data images
 """
 
 import keras, os
-from keras.layers import Dense, Activation, Flatten, Conv2D
+from keras.layers import (Dense, Activation, 
+                    Flatten, Conv2D, MaxPooling2D, Dropout)
 from keras.models import Sequential, load_model
 
- # project modules
+# project modules
 from .. import config
 
- #defining CNN model
-def get_model():
-     model = Sequential()
-     model.add(Conv2D(32, (3, 3), padding = "same",
-                input_shape = config.img_shape))
 
+#defining CNN model
+def get_model():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), padding = "same",
+                input_shape = config.img_shape))
     model.add(Activation("relu"))
+
+    model.add(Conv2D(32, (3, 3), padding = "same",
+                input_shape = config.img_shape))
+    model.add(Activation("relu"))
+
+    model.add(MaxPooling2D(pool_size = (2,2)))
+    model.add(Dropout(rate = 0.20))
+
+    model.add(Conv2D(64, (3, 3), padding = "same",
+                input_shape = config.img_shape))
+    model.add(Activation("relu"))
+
+    model.add(Conv2D(64, (3, 3), padding = "same",
+                input_shape = config.img_shape))
+    model.add(Activation("relu"))
+
+    model.add(MaxPooling2D(pool_size = (2,2)))
+    model.add(Dropout(rate = 0.20))
+
+    model.add(Flatten())
+    model.add(Dense(384, 
+                kernel_regularizer= keras.regularizers.l2(0.01)))
+    model.add(Activation("relu"))
+
+    model.add(Dropout(rate = 0.30))
+    model.add(Dense(config.nb_classes))
+    model.add(Activation("softmax"))
 
     return model
 
 
 
 
-
-    if __name__ == "__main__":
-        m = get_model()
-        m.summary()
+if __name__ == "__main__":
+    m = get_model()
+    m.summary()
